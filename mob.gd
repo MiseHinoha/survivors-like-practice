6880ef738f245b1172573be7c7631064f024a63f
MiseHinoha <13265384114@163.com>
2024-02-07 13:36:02 +0800

    增加怪物死亡动画播放

--- mob.gd
+++ mob.gd
@@ -20,3 +20,7 @@ func take_damage():
 	# 判断角色血量
 	if max_health == 0:
 		queue_free()
+		const SMOKE_SCENE = preload("res://smoke_explosion/smoke_explosion.tscn")
+		var smoke = SMOKE_SCENE.instantiate()
+		get_parent().add_child(smoke)
+		smoke.global_position = global_position
